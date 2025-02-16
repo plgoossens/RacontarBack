e2adc349107280790cfe1654a5bfe7d6af28ce13
plgoossens <pierre.louis.goossens@gmail.com>
2025-02-16 11:40:04 +0100

    Add log

--- src/main/java/be/plgoosse/racontarback/controller/StoryController.java
+++ src/main/java/be/plgoosse/racontarback/controller/StoryController.java
@@ -3,6 +3,8 @@ package be.plgoosse.racontarback.controller;
 import be.plgoosse.racontarback.dto.StoryCreationDTO;
 import be.plgoosse.racontarback.dto.StoryDTO;
 import be.plgoosse.racontarback.service.StoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,11 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/story")
 public class StoryController {
 
+    private Logger logger = LoggerFactory.getLogger(StoryController.class.getName());
+
     @Autowired
     private StoryService storyService;
 
     @PostMapping
     public ResponseEntity<StoryDTO> createStory(@RequestBody StoryCreationDTO storyCreationDTO){
+        logger.info("Creating story...");
         return ResponseEntity.ok(storyService.createStory(storyCreationDTO));
     }
 }
